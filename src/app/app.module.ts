c9386ee81d7b9119aa97d8fc6fa4b5ad9f504e43
Arthur Christoph <achristoph@gmail.com>
2019-03-30 13:23:18 -0500

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { WebSocketService } from './websocket.service';
-import { HomeComponent } from './home/home.component';
 import { DetailsComponent } from './details/details.component';
+import { HomeComponent } from './home/home.component';
+import { WebSocketService } from './websocket.service';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, DetailsComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, ReactiveFormsModule],
   providers: [WebSocketService],
   bootstrap: [AppComponent]
 })
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { Subject, Subscription } from 'rxjs';
 import { WebSocketService } from '../websocket.service';
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -11,27 +11,33 @@ export class HomeComponent implements OnInit {
   messages = [];
   clientSocket: Subject<any>;
   subscription: Subscription;
-  MAX_MESSAGE = 10;
-  constructor(private ws: WebSocketService) {}
+  MAX_MESSAGE = 5;
+  messageForm;
+  constructor(private fb: FormBuilder, private ws: WebSocketService) {}
 
   ngOnInit() {
+    this.messageForm = this.fb.group({
+      name: ['']
+    });
+
     const url = 'ws://localhost:8181/';
     if (!this.ws.ws) {
       console.log('Client connecting to Websocket server on port 8181');
       this.clientSocket = this.ws.connect(url);
+      console.log(this.clientSocket);
       this.subscription = this.clientSocket.subscribe(m => {
-        console.log(m);
+        if (this.messages.length === this.MAX_MESSAGE) {
+          this.messages.shift();
+        }
         this.messages = [...this.messages, m.data];
       });
     }
   }
   sendMessage(m) {
+    console.log(this.messageForm.value);
     console.log(`Client sending message: ${m}`);
-    this.clientSocket.next(m);
-    if (this.messages.length === this.MAX_MESSAGE) {
-      this.messages.shift();
-    }
-    // this.messages = [...this.messages, m];
+    const payload = { message: m, ...this.messageForm.value };
+    this.clientSocket.next(payload);
   }
 
   closeConnection() {
